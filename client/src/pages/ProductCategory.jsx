46ffa5bca62992afb10cc7223c060bac73441f6a
Maksat Alymkulov <maksat.alymkulov@gmail.com>
2025-05-08 22:15:59 -0700

    fixed resposiveness for phones

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -17,28 +17,31 @@ const ProductCategory = () => {
   );
 
   return (
-    <div className="mt-16">
-      {searchCategory && (
-        <div className="flex flex-col items-end w-max">
-          <p className="text-2xl font-medium">
-            {searchCategory.text.toUpperCase()}
-          </p>
-          <div className="w-16 h-0.5 bg-primary rounded-full"></div>
-        </div>
-      )}
-      {filteredProducts.length > 0 ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6">
-          {filteredProducts.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-        </div>
-      ) : (
-        <div className="flex items-center justify-center h-[60vh]">
-          <p className="text-2xl font-medium text-primary">
-            No products found in this category.
-          </p>
-        </div>
-      )}
+    <div className="w-full px-4 sm:px-6 md:px-8 mt-16">
+      <div className="max-w-screen-xl mx-auto">
+        {searchCategory && (
+          <div className="flex flex-col items-end">
+            <p className="text-2xl font-medium">
+              {searchCategory.text.toUpperCase()}
+            </p>
+            <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+          </div>
+        )}
+
+        {filteredProducts.length > 0 ? (
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 mt-6">
+            {filteredProducts.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-[60vh]">
+            <p className="text-2xl font-medium text-primary">
+              No products found in this category.
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
